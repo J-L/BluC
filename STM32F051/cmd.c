dfdef4b9f54eb5d3f881829466ae182183beeb3d
J-L <johnlee@johnlee.net>
2014-03-02 06:27:47 -0500

    added some prototype command

--- STM32F051/cmd.c
+++ STM32F051/cmd.c
@@ -14,3 +14,44 @@ void cmdGetTemp(BaseSequentialStream *chp, int argc, char *argv[])
 
 }
 
+void cmdGetVoltage(BaseSequentialStream *chp, int argc, char *argv[]) 
+{
+    chprintf(chp, "Hello World");  
+
+}
+
+void cmdGetBattery(BaseSequentialStream *chp, int argc, char *argv[]) 
+{
+    chprintf(chp, "Hello World");  
+
+}
+
+void cmdAdc(BaseSequentialStream *chp, int argc, char *argv[]) 
+{
+    chprintf(chp, "Hello World");  
+
+}
+
+void cmdPwm(BaseSequentialStream *chp, int argc, char *argv[]) 
+{
+    chprintf(chp, "Hello World");  
+
+}
+
+void cmdUart(BaseSequentialStream *chp, int argc, char *argv[]) 
+{
+    chprintf(chp, "Hello World");  
+
+}
+
+void cmdSpi(BaseSequentialStream *chp, int argc, char *argv[]) 
+{
+    chprintf(chp, "Hello World");  
+
+}
+
+void cmdBluetooth(BaseSequentialStream *chp, int argc, char *argv[]) 
+{
+    chprintf(chp, "Hello World");  
+
+}
